cc6080c260251269411c96a43dcfdd53bbecd0ec
Richard Blythman <richardblythman@gmail.com>
2024-04-19 13:16:04 +0100

    use litellm

--- chat/run.py
+++ chat/run.py
@@ -1,43 +1,44 @@
-import requests
+#!/usr/bin/env python
+
 from chat.schemas import InputSchema
 from chat.utils import get_logger
 import json
+from litellm import completion
+import yaml
 
 logger = get_logger(__name__)
 
-OLLAMA_ENDPOINT = 'http://localhost:11434/api/generate'
-
-
-def run(job: InputSchema, cfg: dict = None, **kwargs):
-    logger.info(f"Running job {job.model} {job.prompt}")
+def run(inputs: InputSchema, cfg: dict = None, **kwargs):
+    logger.info(f"Running with inputs {inputs.prompt}")
     logger.info(f"cfg: {cfg}")
 
-
-    data = {
-        'model': job.model,
-        'prompt': job.prompt,
-        'stream': False
-    }
-
-    response = requests.post(
-        OLLAMA_ENDPOINT,
-        json=data
+    prompt = inputs.prompt
+    messages = [
+        {"role": "system", "content": cfg["inputs"]["system_message"]},
+        {"role": "user", "content": prompt},
+    ]
+    response = completion(
+        model=cfg["models"]["ollama"]["model"],
+        messages=messages,
+        temperature=cfg["models"]["ollama"]["temperature"],
+        max_tokens=cfg["models"]["ollama"]["max_tokens"],
+        api_base=cfg["models"]["ollama"]["api_base"],
     )
 
-    response.raise_for_status()
-    response_json = response.json()
+    response = response.choices[0].message["content"]
+    logger.info(f"Response: {response}")
 
-    logger.info(f'RESPONSE JSON: {response_json}')
-
-
-    return response_json['response']
+    return response
 
 if __name__ == "__main__":
-    job = InputSchema(
-        model='mistral',
+
+    cfg_path = f"chat/component.yaml"
+    with open(cfg_path, "r") as file:
+        cfg = yaml.load(file, Loader=yaml.FullLoader)
+
+    inputs = InputSchema(
         prompt='tell me a joke',
     )
-    response = run(job)
+    response = run(inputs, cfg)
 
-    print('ttttttt', response)
 
--- chat/schemas.py
+++ chat/schemas.py
@@ -1,12 +1,4 @@
 from pydantic import BaseModel
-from enum import Enum
-
-class Model(str, Enum):
-    MISTRAL = 'mistral'
-    GEMMA = 'gemma'
-    QWEN = 'qwen'
-
 
 class InputSchema(BaseModel):
     prompt: str
-    model: Model = Model.MISTRAL
